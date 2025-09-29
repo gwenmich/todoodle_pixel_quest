7d16b2b0de42081fca05143b608856796ee1a2a7
Gwen Michailidis <79330147+gwenmich@users.noreply.github.com>
2025-09-29 15:04:31 +0100

    Implement home index view

--- app/helpers/quests_helper.rb
+++ app/helpers/quests_helper.rb
@@ -1,2 +1,12 @@
 module QuestsHelper
+
+  def instructions
+    [
+      "Forge new quests worthy of thy valor",
+      "Add noble tasks to each sacred quest",
+      "Mark tasks complete as thy triumph",
+      "Watch thy knight advance towards glory"
+    # "Reach the ancient castle to claim victory!" Uncomment when progress bar set up
+    ]
+  end
 end
